8d097552538eb78ace29d35074a2ea71454a9ac7
Davor Barishic-Jaman <cro.davor@outlook.com>
2019-07-27 23:13:24 +0200

    Remove S3 related code.

--- SpeakeasyFunctions/src/main/java/com/dbarishic/speakeasy/ListAvailableLanguagesFunction.java
+++ SpeakeasyFunctions/src/main/java/com/dbarishic/speakeasy/ListAvailableLanguagesFunction.java
@@ -1,5 +1,8 @@
 package com.dbarishic.speakeasy;
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
@@ -10,7 +13,9 @@ import software.amazon.awssdk.services.polly.model.DescribeVoicesRequest;
 import software.amazon.awssdk.services.polly.model.DescribeVoicesResponse;
 import software.amazon.awssdk.services.polly.model.Voice;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,21 +24,38 @@ public class ListAvailableLanguagesFunction {
 
     private static Logger log = LoggerFactory.getLogger(ListAvailableLanguagesFunction.class);
 
-    private PollyClient client = PollyClient.builder()
+    private final PollyClient client = PollyClient.builder()
             .credentialsProvider(DefaultCredentialsProvider.create())
             .region(Region.EU_WEST_1)
             .httpClient(ApacheHttpClient.builder().build())
             .build();
 
-    public Response handleRequest(final Request request) {
-        Set<String> voices = getlanguageNames();
-        return new Response(voices.toString(), 200);
+    public APIGatewayProxyResponseEvent handleRequest() {
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final Set<String> languages = getlanguageNames();
+        final Map<String, Set<String>> data = new HashMap<>();
+        data.put("languages", languages);
+
+        String dataJson = null;
+        try {
+            dataJson = mapper.writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            log.debug("context", e);
+        }
+
+        final APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setBody(dataJson);
+        response.setIsBase64Encoded(false);
+        response.setStatusCode(200);
+
+        return response;
     }
 
     private Set<String> getlanguageNames() {
-        DescribeVoicesRequest allVoicesRequest = DescribeVoicesRequest.builder().build();
+        final DescribeVoicesRequest allVoicesRequest = DescribeVoicesRequest.builder().build();
 
-        Set<Voice> voices = new HashSet<>();
+        final Set<Voice> voices = new HashSet<>();
         Set<String> languageNames = new HashSet<>();
         try {
             String nextToken;
--- SpeakeasyFunctions/src/main/java/com/dbarishic/speakeasy/SynthesizeSpeechFunction.java
+++ SpeakeasyFunctions/src/main/java/com/dbarishic/speakeasy/SynthesizeSpeechFunction.java
@@ -1,28 +1,30 @@
 package com.dbarishic.speakeasy;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.core.exception.SdkClientException;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.polly.PollyClient;
 import software.amazon.awssdk.services.polly.model.OutputFormat;
 import software.amazon.awssdk.services.polly.model.SynthesizeSpeechRequest;
 import software.amazon.awssdk.services.polly.model.VoiceId;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.concurrent.ExecutionException;
+import java.util.Base64;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class SynthesizeSpeechFunction {
+public class SynthesizeSpeechFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private static Logger log = LoggerFactory.getLogger(SynthesizeSpeechFunction.class);
 
@@ -32,38 +34,49 @@ public class SynthesizeSpeechFunction {
             .httpClient(ApacheHttpClient.builder().build())
             .build();
 
-    private final S3Client myS3Client = S3Client.builder()
-            .credentialsProvider(DefaultCredentialsProvider.create())
-            .region(Region.EU_WEST_1)
-            .httpClient(ApacheHttpClient.builder().build())
-            .build();
+    // Synthesize speech and return it as a base64 encoded string
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest (APIGatewayProxyRequestEvent requestEvent, Context context) {
+        final ObjectMapper mapper = new ObjectMapper();
 
-    public Response handleRequest(final Request request) throws ExecutionException, InterruptedException {
+        // deconstruct request
+        Request request = new Request();
 
-        // Synthesize speech and save to a new mp3 file
-        SynthesizeSpeechRequest synthesizeSpeechRequest = SynthesizeSpeechRequest.builder()
+        try {
+            request = mapper.readValue(requestEvent.getBody(), Request.class);
+        } catch (IOException e) {
+            log.debug("context", e);
+        }
+
+        final SynthesizeSpeechRequest synthesizeSpeechRequest = SynthesizeSpeechRequest.builder()
                 .outputFormat(OutputFormat.MP3)
                 .voiceId(VoiceId.JOANNA)
                 .text(request.getMessage())
                 .build();
 
-        String fileName = "tmp/" + LocalDateTime.now().toString() + request.getLanguage() + ".mp3";
-        File outputFile = new File(fileName);
-        myPollyClient.synthesizeSpeech(synthesizeSpeechRequest, outputFile.toPath());
+        final String fileName = "/tmp/" + LocalDateTime.now().toString() + ".mp3";
+        final File synthesizedSpeechMP3 = new File(fileName);
 
-        // Upload generated mp3 to S3 bucket
-        String myGeneratedMp3Url = null;
+        myPollyClient.synthesizeSpeech(synthesizeSpeechRequest, synthesizedSpeechMP3.toPath());
+
+        // construct response
+        final APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        final String synthesizedSpeechBase64String = fileToBase64(synthesizedSpeechMP3);
+        response.setIsBase64Encoded(true);
+        response.setBody(synthesizedSpeechBase64String);
+        response.setStatusCode(200);
+
+        return response;
+    }
+
+    private String fileToBase64(File file) {
+        byte[] synthesizedSpeechMP3AsByteArray = new byte[0];
         try {
-            String bucketName = System.getenv("BUCKET_NAME");
-
-            myS3Client.putObject(PutObjectRequest.builder().bucket(bucketName).key(fileName).acl(ObjectCannedACL.PUBLIC_READ).build(),
-                    RequestBody.fromFile(outputFile));
-
-            myGeneratedMp3Url = S3Helper.getUrl(bucketName, fileName);
-        } catch (SdkClientException e) {
+            synthesizedSpeechMP3AsByteArray = FileUtils.readFileToByteArray(file);
+        } catch (IOException e) {
             log.debug("context", e);
-            return new Response("Error while processing request", 500);
         }
-        return new Response(myGeneratedMp3Url, 200);
+
+        return Base64.getEncoder().encodeToString(synthesizedSpeechMP3AsByteArray);
     }
 }
