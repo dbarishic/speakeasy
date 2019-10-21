2a44d3372885cdb803ffdc443d631b17200f1d99
Davor Barishic-Jaman <davor.barishikj-jaman@intertec.io>
2019-10-21 16:39:46 +0200

    init project for espeak-backed speech synthesis

---
+++ speakeasy-espeak/src/main/java/com/dbarihic/speakeasyespeak/SpeakeasyEspeakApplication.java
@@ -0,0 +1,13 @@
+package com.dbarihic.speakeasyespeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakeasyEspeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakeasyEspeakApplication.class, args);
+	}
+
+}
